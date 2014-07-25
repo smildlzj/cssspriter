4210c86ab15bf53c4fbe8936ad508f5cd62b38e6
luzhujun <luzhujun@rd.netease.com>
2014-07-25 15:50:50 +0800

    update

---
+++ cssspriter.js
@@ -0,0 +1,10 @@
+var css = require("./lib/css");
+
+
+//main
+//css.add("test1/t.css");
+//css.process();
+
+
+exports.add = css.add;
+exports.process = css.process;
--- sprite.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-    path = require('path'),
-    css = require("./lib/css"),
-    io = require("./lib/io");
-
-
-//main
-css.add("test1/t.css");
-css.process();
