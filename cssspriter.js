579b20c89df127253e1fe52c9aa55802b2fd892b
luzhujun <luzhujun@rd.netease.com>
2014-07-26 23:43:05 +0800

    MOD:add sprite history

--- cssspriter.js
+++ cssspriter.js
@@ -1,10 +1,45 @@
-var css = require("./lib/css");
+var 
+    path = require('path'),
+    css = require("./lib/css"),
+    io = require("./lib/io"),
+    history = require("./lib/history");
 
 
 //main
 //css.add("test1/t.css");
 //css.process();
 
+var process = function(file){
+    var dir;
+    if(!file){
+        //当前目录
+        dir = __dirname + "/";
+        file =  history.load(dir).spriteImgName;
+    }else if(io.isDirectory(file)){
+        //指定目录
+        dir = file + "/";
+        file =  history.load(dir).spriteImgName;
+    }
+
+    //未指定文件名,自动生成
+    if(!file){
+        for(var i = 1 ;;i++){
+            file = dir + "sprite"+i+".png";
+            if(!io.exist(file)){
+                break;
+            }
+        }
+    }else{
+        file = dir + file;
+    }
+
+    css.process(file);
+
+    //保存历史记录
+    history.save({
+        spriteImgName : path.relative(dir , file)
+    });
+};
 
 exports.add = css.add;
-exports.process = css.process;
+exports.process = process;
